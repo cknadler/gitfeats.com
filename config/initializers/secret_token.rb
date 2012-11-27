dadb06feea9c25572c634ad982aed2e582e0d9f9
Chris Knadler <takeshi91k@gmail.com>
2012-11-26 17:42:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitfeats::Application.config.secret_token = 'b6b0f595f4f8cd061dbdd4621ee6482e33b2422e1a502cc0860f08c16d34aef9f23f56a7946cf42ed953fa84da1c65a1dc81e33a38b026f545444dc37645eb7d'

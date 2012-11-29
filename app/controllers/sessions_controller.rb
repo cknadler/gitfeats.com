af8c6edfbe86b070aa0d3d0152458f44ea5967af
ieckert <ieckert@mail.csuchico.edu>
2012-11-29 14:59:59 -0800

    making sure we are up to date

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 class SessionsController < ApplicationController    
     def new
 
@@ -6,11 +8,12 @@ class SessionsController < ApplicationController
 #where the callback comes in
     def create
       auth_hash = request.env['omniauth.auth']
+#      @user = get_user_info(auth_hash["extra"]["url"].split('/').last)
       @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
       if @authorization
         render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
       else
-        render :text => auth_hash.inspect
+        render :text => auth_hash["extra"]["info"].inspect
 =begin        
         user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
         user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
@@ -28,5 +31,8 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         redirect_to root_url, :notice => "Logged out"
     end
-
+    def get_user_info (name)
+      url = "http://github.com/api/v1/json" + "/#{name}"
+        JSON.parse(open(url).read)["user"]
+    end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   file = YAML.load_file("config/git_secret.yml")
   secret = file["secret"] 
   id = file["id"]
-  provider :github, id, secret, scope: "user"
+  provider :github, id, secret
 end

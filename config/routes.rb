e3c5269a6bc4e955ef0347ba1232910e306a4dfd
ieckert <ieckert@mail.csuchico.edu>
2012-12-02 15:02:39 -0800

    corrected login/logout routes and added links on the topbar for them

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Gitfeats::Application.routes.draw do
   root to: 'public_pages#home'
 
-  get   '/login', :to => 'sessions#new', :as => :login
-  match '/auth/:provider/callback', :to => 'sessions#create'
-  match '/auth/failure', :to => 'sessions#failure'
-  match 'logout'   => "sessions#destroy", :via => :delete
+  get   'login',                  :to => 'sessions#new'
+  match '/auth/github/callback',  :to => 'sessions#create'
+  match '/auth/failure',          :to => 'sessions#failure'
+  match 'logout',                 :to => 'sessions#destroy', :via => :delete
 end

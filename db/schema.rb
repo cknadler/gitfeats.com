a4b0b82fd17be38f7c821927b8b547be4f8a91c4
Chris Knadler <takeshi91k@gmail.com>
2012-12-02 16:59:35 -0800

    Update schema and remove feats scaffold chuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202233617) do
+ActiveRecord::Schema.define(:version => 20121203002126) do
+
+  create_table "feats", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "completed_count"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.integer  "uid"

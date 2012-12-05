5471bfcebe1318640623e4c9dd458934acbb185a
dasmoose <jennifer.coryell@gmail.com>
2012-12-04 23:43:46 -0800

    Adds set up page and connects start here to set up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Gitfeats::Application.routes.draw do
 
   root to: 'public_pages#home'
+  match '/setup',                  to: 'public_pages#setup'
 
   get   'login',                  to: 'sessions#new'
   match '/auth/github/callback',  to: 'sessions#create'

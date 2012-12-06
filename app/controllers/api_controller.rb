2ce1618597bc7da25d556dad35fb7780c44d62fd
ieckert <ieckert@mail.csuchico.edu>
2012-12-06 13:38:35 -0800

    the seeds file will now populate the commands and feats tables in the db, tested updating command history - works

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,29 @@
 class ApiController < ApplicationController
-
+  before_filter :load_user
+  
   def post_feats
-    user = User.find_by_nickname(params[:username])
-    params[:history].each do |h_name, h_count|
-      command = Command.find_by_name(h_name)
-      ch = CommandHistory.find_or_create_by_user_id_and_command_id(user.id, command.id)
-      ch.count = h_count
-      ch.save
-    end
+    update_history( params[:history] )
+    complete_feats
+    
+
     render :nothing => true
   end
+  
+  private
+    def load_user
+      @user = User.find_by_nickname(params[:username])
+    end
+    
+    def update_history( histories )
+        histories.each do |h_name, h_count|
+        command = Command.find_by_name(h_name)
+        ch = CommandHistory.find_or_create_by_user_id_and_command_id(@user.id, command.id)
+        ch.count = h_count
+        ch.save
+      end
+    end
+    
+    def complete_feats
+      
+    end
 end

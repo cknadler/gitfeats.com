0005ae1b8a2185e4d45c4ff3e6b2decdbf1865d0
Chris Knadler <takeshi91k@gmail.com>
2012-12-08 15:43:57 -0800

    Implements conditional root_path routing Root goes to public_pages#home unless the user is signed in. In that case, root goes to public_pages#stats. This solves a huge amount problems with flashes and general style, making the public_pages#home basically a splash page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Gitfeats::Application.routes.draw do
 
-  root to: 'public_pages#home'
+  # Public Pages
+  root            to: 'public_pages#home'
+  match '/stats', to: 'public_pages#stats'
 
+  # Sessions
   get   'login',                  to: 'sessions#new'
   match '/auth/github/callback',  to: 'sessions#create'
   match '/auth/failure',          to: 'sessions#failure'
   match 'logout',                 to: 'sessions#destroy', :via => :delete
+
+  # Users
   resources :users, only: [:show, :destroy]
 
+  # Feats
+  resources :feats, only: [:show, :index]
+
+  # API
   scope '/api' do
     match '/post_feats', to: 'api#post_feats', :via => :post
   end
 
-  resources :feats, only: [:show, :index]
 end

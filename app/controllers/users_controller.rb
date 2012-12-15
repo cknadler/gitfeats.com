7aa7bc44e161c057d3575fe7c5c853169c8be4d8
Chris Knadler <takeshi91k@gmail.com>
2012-12-15 00:10:54 -0800

    Fixes several dead links and general code cleanup
    commit ec343301a8d1c37296d6ebc0f1ad4340f41f1fcc
    Author: Chris Knadler <takeshi91k@gmail.com>
    Date:   Sat Dec 15 00:09:54 2012 -0800
    
        Fixes links on public_pages#home
    
    commit 280a23f458363664f3e347f06eb97c3bde4e7de3
    Author: Chris Knadler <takeshi91k@gmail.com>
    Date:   Fri Dec 14 14:22:36 2012 -0800
    
        Cleans up a few bits in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @github_url = "http://www.github.com/#{@user.nickname}" 
 
     # Setup data
-    @api_key = "#{@user.gemkey}"
+    @api_key = @user.gemkey
     @install_cmd = "gem install git-feats"
     @key_cmd = format_key_conf_command(@api_key)
     @user_cmd = format_user_conf_command(@user.nickname)
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
 
     # else if the search for user fails, go back and tell them
     elsif @user.nil?
-      redirect_to(:back, :notice => "Could not find user #{params[:query]}")
+      redirect_to(:back, 
+                  notice: "Could not find user #{params[:query]}")
 
     # else take them to the users page
     else

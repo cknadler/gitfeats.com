b8916cf284c32b45d8c2989a763bcddc4e3a836b
Chris Knadler <takeshi91k@gmail.com>
2012-12-16 20:14:11 -0800

    static -> public pages spec Makes everything work correctly with guard

--- spec/requests/public_pages_spec.rb
+++ spec/requests/public_pages_spec.rb
@@ -14,16 +14,5 @@ describe "static pages" do
     it { should have_link 'Gem on GitHub', 
          :href => 'https://github.com/cknadler/git-feats' }
   end
-
-  describe 'footer' do
-    before do
-      visit root_path
-    end
-
-    it { should have_link 'Git Help', :href => 'http://git-scm.com' }
-    it { should have_link 'Fork the Site',
-         :href => 'https://github.com/cknadler/gitfeats.com' }
-    it { should have_link 'Fork the Gem',
-         :href => 'https://github.com/cknadler/git-feats' }
-  end
 end
+
